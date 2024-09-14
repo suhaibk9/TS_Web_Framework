aaf96daebf2a2245c9daf858b3d361ab27a7a03b
Suhaib Khan <suhaib0900@gmail.com>
2024-09-14 15:59:32 +0100

    Started Using Composition

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -1,7 +1,7 @@
-import axios, { AxiosResponse } from "axios";
-import { Eventing } from "./Eventing";
-import { Sync } from "./Sync";
-import { Attributes } from "./Attributes";
+import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 export interface UserProps {
   id?: number;
   name?: string;
@@ -17,5 +17,39 @@ export class User {
 
   constructor(attrs: UserProps) {
     this.attributes = new Attributes<UserProps>(attrs);
+  }
+  //Eventing
+  get on() {
+    return this.events.on;
+  }
+  get trigger() {
+    return this.events.trigger;
+  }
+  //Attributes
+  get get() {
+    return this.attributes.get;
+  }
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+  fetch(): void {
+    const id = this.attributes.get('id');
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+  save(): void {
+    this.sync
+      .save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { User } from './Models/User';
-const user = new User({ name: 'new record', age: 0 });
-user.syn
+const user = new User({ id: 1 });
 
+user.on('change', () => {
+  console.log('User was changed');
+  console.log(user);
+});
+user.fetch();
